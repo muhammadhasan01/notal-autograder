6914c88fa48445fa48ed47fbe6716ded44dc10e8
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2022-02-02 10:07:54 +0700

    feat: add get count edges

--- cfg_grader/utils/helpers.py
+++ cfg_grader/utils/helpers.py
@@ -1,10 +1,17 @@
-def get_count_out(cfg):
+def get_count_out(g):
     count_out = {}
-    for node in cfg:
-        for adj_node in cfg[node]:
+    for node in g:
+        for adj_node in g[node]:
             label = adj_node.get_label()
             if label not in count_out:
                 count_out[label] = 1
             else:
                 count_out[label] += 1
     return count_out
+
+
+def get_count_edges(g):
+    ret = 0
+    for u in g:
+        ret += len(u.get_adjacent())
+    return ret
