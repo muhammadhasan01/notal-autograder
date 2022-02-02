9c0fcc5e4af043d1bb2bc6a31785888a3231574f
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2022-02-02 10:08:19 +0700

    feat: add compare graph function

---
+++ cfg_grader/utils/compare_graph.py
@@ -0,0 +1,26 @@
+from munkres import Munkres
+
+from cfg_grader.utils.constants import Constants
+from cfg_grader.utils.cost_matrix import create_cost_matrix
+from cfg_grader.utils.helpers import get_count_edges
+
+
+def compare(g1, g2):
+    munkres = Munkres()
+    cost_matrix = create_cost_matrix(g1, g2)
+    indexes = munkres.compute(cost_matrix)
+
+    total = 0
+    details = []
+    for i, j in indexes:
+        val = cost_matrix[i][j]
+        total += val
+        details.append([i, j, val])
+
+    node1 = len(g1)
+    node2 = len(g2)
+    edge1 = get_count_edges(g1)
+    edge2 = get_count_edges(g2)
+    score = (1 - (total / (node1 + node2 + edge1 + edge2))) * Constants.MAX_SCORE
+
+    return score, total, details
