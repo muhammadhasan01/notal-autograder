2c58ab587a3d845d6e7c0de9960a2a6c6953d080
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2022-02-02 10:07:40 +0700

    refactor: rename cfg to g

--- cfg_grader/utils/graph_collapser.py
+++ cfg_grader/utils/graph_collapser.py
@@ -1,16 +1,16 @@
 from cfg_grader.utils.dsu import DSU
 
 
-def collapse(cfg):
-    dsu = DSU(len(cfg))
+def collapse(g):
+    dsu = DSU(len(g))
 
     # Merge nodes with the same flow
-    for node in cfg:
-        if len(cfg[node]) > 1:
+    for node in g:
+        if len(g[node]) > 1:
             continue
         label = node.get_label()
-        for adj_node in cfg[node]:
-            if len(cfg[adj_node]) > 1:
+        for adj_node in g[node]:
+            if len(g[adj_node]) > 1:
                 continue
             adj_label = adj_node.get_label()
             if len(adj_node.get_in_nodes()) == 1:
@@ -18,7 +18,7 @@ def collapse(cfg):
 
     # Store information from merged nodes
     add_infos = {}
-    for node in cfg:
+    for node in g:
         label = node.get_label()
         par_label = dsu.find_par(label)
         if label == par_label:
@@ -29,7 +29,7 @@ def collapse(cfg):
             add_infos[par_label].append(info)
 
     # Add information to parent node
-    for node in cfg:
+    for node in g:
         label = node.get_label()
         if label in add_infos:
             for info in add_infos[label]:
@@ -37,11 +37,11 @@ def collapse(cfg):
 
     # Get new adjacency list and store label to node data
     new_adj, label_to_node = {}, {}
-    for node in cfg:
+    for node in g:
         label = node.get_label()
         label_to_node[label] = node
         par_label = dsu.find_par(label)
-        for adj_node in cfg[node]:
+        for adj_node in g[node]:
             adj_label = adj_node.get_label()
             par_adj_label = dsu.find_par(adj_label)
             if dsu.check_same(par_label, par_adj_label):
@@ -50,18 +50,18 @@ def collapse(cfg):
                 new_adj[par_label] = []
             new_adj[par_label].append(par_adj_label)
 
-    # Get new CFG
-    new_cfg = {}
-    for node in cfg:
+    # Get new graph
+    new_g = {}
+    for node in g:
         label = node.get_label()
         if label == dsu.find_par(label):
-            new_cfg[node] = []
+            new_g[node] = []
             if label in new_adj:
                 for adj_label in new_adj[label]:
-                    new_cfg[node].append(label_to_node[adj_label])
+                    new_g[node].append(label_to_node[adj_label])
 
     # Re-Enumerate Label
-    for idx, node in enumerate(new_cfg):
+    for idx, node in enumerate(new_g):
         node.set_label(idx + 1)
 
-    return new_cfg
+    return new_g
