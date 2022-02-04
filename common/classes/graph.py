11619d33ea5d2cf8702356e84235a013aab6888b
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2022-02-04 20:21:33 +0700

    chore: remove self edges in Graph Class

--- common/classes/graph.py
+++ common/classes/graph.py
@@ -4,21 +4,21 @@ from common.classes.node import Node
 class Graph:
     def __init__(self, cfg=None):
         self.nodes: list[Node] = []
-        self.edges: list[tuple[Node, Node]] = []
         if cfg is not None:
             self.build_from_cfg_graph(cfg)
 
     def add_node(self, node: Node):
         self.nodes.append(node)
 
-    def add_edges(self, u: Node, v: Node):
-        self.edges.append((u, v))
+    def get_edges(self):
+        edges: list[tuple[Node, Node]] = []
+        for u in self.nodes:
+            for v in u.get_adjacent():
+                edges.append((u, v))
+        return edges
 
     def get_nodes(self):
         return self.nodes
-
-    def get_edges(self):
-        return self.edges
 
     def set_nodes(self, nodes: list[Node]):
         self.nodes = nodes
@@ -26,8 +26,6 @@ class Graph:
     def build_from_cfg_graph(self, cfg: dict):
         for node in cfg:
             self.add_node(node)
-            for adj_node in cfg[node]:
-                self.add_edges(node, adj_node)
 
     def generate_to_cfg_graph(self):
         cfg = {}
