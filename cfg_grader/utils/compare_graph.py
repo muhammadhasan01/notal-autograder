1042ea6aed10a31bfa8cc22dcff6d4c1a394ff58
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2022-02-04 21:26:18 +0700

    refactor: use Graph Class instead

--- cfg_grader/utils/compare_graph.py
+++ cfg_grader/utils/compare_graph.py
@@ -2,10 +2,10 @@ from munkres import Munkres
 
 from common.classes.constants import Constants
 from cfg_grader.utils.cost_matrix import create_cost_matrix
-from cfg_grader.utils.helpers import get_count_edges
+from common.classes.graph import Graph
 
 
-def compare(g1, g2):
+def compare(g1: Graph, g2: Graph):
     munkres = Munkres()
     cost_matrix = create_cost_matrix(g1, g2)
     indexes = munkres.compute(cost_matrix)
@@ -17,10 +17,10 @@ def compare(g1, g2):
         total += val
         details.append([i, j, val])
 
-    node1 = len(g1)
-    node2 = len(g2)
-    edge1 = get_count_edges(g1)
-    edge2 = get_count_edges(g2)
+    node1 = len(g1.get_nodes())
+    node2 = len(g2.get_nodes())
+    edge1 = len(g1.get_edges())
+    edge2 = len(g2.get_edges())
     score = (1 - (total / (node1 + node2 + edge1 + edge2))) * Constants.MAX_SCORE
 
     return score, total, details
--- cfg_grader/utils/cost_matrix.py
+++ cfg_grader/utils/cost_matrix.py
@@ -1,20 +1,22 @@
 from common.classes.constants import Constants
+from common.classes.graph import Graph
 
 
 def get_cost_real(a: int, b: int):
     return a + b - (2 * min(a, b))
 
 
-def create_cost_matrix(g1, g2):
+def create_cost_matrix(g1: Graph, g2: Graph):
     # Get number of nodes
-    n, m = len(g1), len(g2)
+    nodes1, nodes2 = g1.get_nodes(), g2.get_nodes()
+    n, m = len(nodes1), len(nodes2)
 
     # Initialize cost matrix
     cost_matrix = [[0 for _ in range(n + m)] for _ in range(n + m)]
 
     # Fill section 1 (real node to real node)
-    for i, u in enumerate(g1):
-        for j, v in enumerate(g2):
+    for i, u in enumerate(nodes1):
+        for j, v in enumerate(nodes2):
             inp1 = len(u.get_in_nodes())
             inp2 = len(v.get_in_nodes())
             out1 = len(u.get_adjacent())
@@ -22,7 +24,7 @@ def create_cost_matrix(g1, g2):
             cost_matrix[i][j] = get_cost_real(inp1, inp2) + get_cost_real(out1, out2)
 
     # Fill section 2 (delete node in graph 1)
-    for i, u in enumerate(g1):
+    for i, u in enumerate(nodes1):
         for j in range(n):
             if i == j:
                 ie = len(u.get_in_nodes())
@@ -32,7 +34,7 @@ def create_cost_matrix(g1, g2):
                 cost_matrix[i][j + m] = Constants.INF
 
     # Fill Section 3 (delete node in graph 2)
-    for i, u in enumerate(g2):
+    for i, u in enumerate(nodes2):
         for j in range(m):
             if i == j:
                 ie = len(u.get_in_nodes())
