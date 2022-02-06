579a12fcda35cfbb66119ade68b705d9bd70d7f1
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2022-02-06 17:46:47 +0700

    feat: add notal grader

---
+++ cfg_grader/src/grader/notal_grader.py
@@ -0,0 +1,22 @@
+from cfg_grader.src.grader.compare_graph import compare_graph
+from cfg_grader.src.utils.graph_collapser import collapse
+from common.src.classes.constants import Constants
+from common.src.classes.graph import Graph
+from notal_to_cfg_generator.src.api.functions import get_cfg
+
+
+def notal_grader(src_answers: list[str], src: str):
+    ret_score, ret_total, ret_details = -Constants.INF, 0, [[]]
+    graph_src = Graph()
+    graph_src.build_from_cfg_graph(get_cfg(None, src))
+    collapse(graph_src)
+    for src_answer in src_answers:
+        graph_src_answer = Graph()
+        graph_src_answer.build_from_cfg_graph(get_cfg(None, src_answer))
+        collapse(graph_src_answer)
+        score, total, details = compare_graph(graph_src_answer, graph_src)
+        if score > ret_score:
+            ret_score, ret_total, ret_details = score, total, details
+
+    return ret_score, ret_total, ret_details
+
---
+++ cfg_grader/src/test/notal_grader_test.py
@@ -0,0 +1,90 @@
+from cfg_grader.src.grader.notal_grader import notal_grader
+
+src_answers = [
+    """
+PROGRAM demo_1
+KAMUS
+    sum, i, n: integer
+
+    function pangkat(a, n: integer) -> integer
+
+ALGORITMA
+    input(n)
+    i <- 1
+    sum <- 0
+
+    repeat
+        sum <- sum + pangkat(2, i)
+        i <- i + 1
+    until (i = n)
+
+    output(sum)
+
+function pangkat(a, n: integer) -> integer
+
+KAMUS LOKAL
+    res, i: integer
+
+ALGORITMA
+    res <- 1
+    repeat n times
+        res <- res * a
+    -> res
+
+""",
+    """
+PROGRAM demo_1
+
+KAMUS
+    sum, n: integer
+
+    function pangkat(a, n: integer) -> integer
+
+ALGORITMA
+    input(n)
+    sum <- pangkat(2, n + 1) - 1
+    output(sum)
+
+function pangkat(a, n: integer) -> integer
+
+KAMUS LOKAL
+    res, i: integer
+
+ALGORITMA
+    res <- 1
+    repeat n times
+        res <- res * a
+    -> res
+"""
+]
+
+src = """
+PROGRAM demo_1
+
+KAMUS
+    sum, n: integer
+
+    function pangkat(a, n: integer) -> integer
+
+ALGORITMA
+    input(n)
+    sum <- pangkat(2, n + 1) - 1
+    output(sum)
+
+function pangkat(a, n: integer) -> integer
+
+KAMUS LOKAL
+    res, i: integer
+
+ALGORITMA
+    res <- 1
+    repeat n times
+        res <- res * a
+    -> res
+
+"""
+
+if __name__ == "__main__":
+    score, total, details = notal_grader(src_answers, src)
+    print(score)
+
