3c8ccb5af68233f3c6eb12ba0e9f6053ba313191
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2022-02-07 20:14:01 +0700

    chore: add handling exception and change ret score to 0 by default

--- cfg_grader/src/grader/notal_grader.py
+++ cfg_grader/src/grader/notal_grader.py
@@ -1,12 +1,13 @@
 from cfg_grader.src.grader.compare_graph import compare_graph
 from cfg_grader.src.utils.graph_collapser import collapse
-from common.src.classes.constants import Constants
 from common.src.classes.graph import Graph
 from notal_to_cfg_generator.src.api.functions import get_cfg
 
 
 def notal_grader(src_answers: list[str], src: str):
-    ret_score, ret_total, ret_details = -Constants.INF, 0, [[]]
+    if len(src_answers) == 0:
+        raise ValueError("src_answers need to have at least one element")
+    ret_score, ret_total, ret_details = 0.0, 0, [[]]
     graph_src = Graph()
     graph_src.build_from_cfg_graph(get_cfg(None, src))
     collapse(graph_src)
@@ -19,4 +20,3 @@ def notal_grader(src_answers: list[str], src: str):
             ret_score, ret_total, ret_details = score, total, details
 
     return ret_score, ret_total, ret_details
-
