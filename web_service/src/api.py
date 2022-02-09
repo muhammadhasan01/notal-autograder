99335e92edf75ee04c2cef7141800969f85691a2
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2022-02-09 16:46:36 +0700

    feat: add new resource to api

--- web_service/src/api.py
+++ web_service/src/api.py
@@ -3,11 +3,13 @@ from flask_restful import Api
 
 from web_service.src.resources.health_check import HealthCheck
 from web_service.src.resources.notal_grader import NotalGrader
+from web_service.src.resources.notal_to_cfg import NotalToCFG
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(HealthCheck, '/healthcheck')
+api.add_resource(NotalToCFG, '/notal-to-cfg')
 api.add_resource(NotalGrader, '/grade/<string:grade_type>')
 
 if __name__ == '__main__':
