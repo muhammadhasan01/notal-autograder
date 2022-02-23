0e6eeac63b3f93f436e09450611d2182252d4f9f
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-02-23 21:19:51 +0700

    refactor: use get response for healthcheck

--- web_service/src/resources/health_check.py
+++ web_service/src/resources/health_check.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource
 
 from web_service.src.utils.logz import create_logger
+from web_service.src.utils.wrapper import get_response
 
 
 class HealthCheck(Resource):
@@ -9,4 +10,4 @@ class HealthCheck(Resource):
 
     def get(self):
         self.logger.info("receiving health check endpoint")
-        return {'error': False, 'message': 'healthy'}, 200
+        return get_response(err=False, msg='Healthy')
