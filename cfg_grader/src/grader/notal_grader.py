b1bdb69bf1cd6f17d11b4ad6f7c49bbd67b2491e
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-02-23 21:15:48 +0700

    refactor: use src_refs instead of src_answers

--- cfg_grader/src/grader/notal_grader.py
+++ cfg_grader/src/grader/notal_grader.py
@@ -4,16 +4,16 @@ from common.src.classes.graph import Graph
 from notal_to_cfg_generator.src.api.functions import get_cfg
 
 
-def notal_grader(src_answers: list[str], src: str):
-    if len(src_answers) == 0:
-        raise ValueError("src_answers need to have at least one element")
+def notal_grader(src_refs: list[str], src: str):
+    if len(src_refs) == 0:
+        raise ValueError("src_refs need to have at least one element")
     ret_score, ret_total, ret_details = 0.0, 0, [[]]
     graph_src = Graph()
     graph_src.build_from_cfg_graph(get_cfg(None, src))
     collapse(graph_src)
-    for src_answer in src_answers:
+    for src_ref in src_refs:
         graph_src_answer = Graph()
-        graph_src_answer.build_from_cfg_graph(get_cfg(None, src_answer))
+        graph_src_answer.build_from_cfg_graph(get_cfg(None, src_ref))
         collapse(graph_src_answer)
         score, total, details = compare_graph(graph_src_answer, graph_src)
         if score > ret_score:
