ece872e4625f957dc9c2886697c94e88d823f787
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-02-23 21:15:14 +0700

    feat: add wrapper

--- web_service/src/utils/wrapper.py
+++ web_service/src/utils/wrapper.py
@@ -1,9 +1,8 @@
-def get_response(err: bool, msg: str, additional: dict[str, any] = None, status_code: int = 200):
+def get_response(err: bool, msg: str, data: dict[str, any] = None, status_code: int = 200):
     ret = {
         "error": err,
         "message": msg
     }
-    if additional:
-        for key, item in additional.items():
-            ret[key] = item
+    if data:
+        ret["data"] = data
     return ret, status_code
