c099f714d17da817bcb733315c4af9ce637690ef
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-02-28 20:17:03 +0700

    chore: remove upload folder and add allowed extensions

--- common/src/classes/constants.py
+++ common/src/classes/constants.py
@@ -4,5 +4,4 @@ import math
 class Constants:
     INF = math.inf
     MAX_SCORE = 100
-    UPLOAD_FOLDER = '/uploads'
-    ALLOWED_EXTENSIONS = {'txt', 'in'}
+    ALLOWED_EXTENSIONS = {'txt', 'in', 'docx'}
