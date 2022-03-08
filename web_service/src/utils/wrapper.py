00ededc7837ea84ae858559ff5eb40a1b26d8bc3
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-03-08 18:32:30 +0700

    refactor: always add data as a response

--- web_service/src/utils/wrapper.py
+++ web_service/src/utils/wrapper.py
@@ -1,8 +1,6 @@
 def get_response(err: bool, msg: str, data: dict[str, any] = None, status_code: int = 200):
-    ret = {
+    return {
         "error": err,
-        "message": msg
-    }
-    if data:
-        ret["data"] = data
-    return ret, status_code
+        "message": msg,
+        "data": data
+    }, status_code
