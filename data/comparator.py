fcae315988b5249359a239451440d79021e8b0af
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-05-04 02:44:08 +0700

    feat: add feature on adding into results csv file

--- data/comparator.py
+++ data/comparator.py
@@ -1,3 +1,4 @@
+import csv
 import os
 
 from graph_grader.src.grader.compare_graph import compare_graph
@@ -10,32 +11,42 @@ ANSWER_NAME_FILE = 'answer'
 EXAM_DIRECTORIES = {
     'Kuis 2 IA': 'Kuis_2_IF2210_2020\\IA'
 }
+HEADER = ['exam_name', 'nim', 'grade']
 
 
 def main():
     logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
     parent_dir = os.path.dirname(os.path.abspath(__file__))
+    results: list[tuple[str, str, str]] = []
+
     for exam_name, dir_exam in EXAM_DIRECTORIES.items():
         logging.info(f'Comparing Answer for exam={exam_name}')
         cur_dir = os.path.join(parent_dir, f'exams\\{dir_exam}')
         input_files = os.listdir(cur_dir)
-        graph_inputs = {}
+        graph_inputs: dict[str, Graph] = {}
         for submission in input_files:
-            name = submission.split('.')[0]
+            nim = submission.split('.')[0]
             cur_path = os.path.join(cur_dir, submission)
             graph = Graph(get_cfg(cur_path))
             collapse(graph)
-            graph_inputs[name] = graph
+            graph_inputs[nim] = graph
 
         if ANSWER_NAME_FILE not in graph_inputs:
             raise FileNotFoundError(f'{ANSWER_NAME_FILE}.txt not found')
 
         graph_answer = graph_inputs[ANSWER_NAME_FILE]
-        for name, graph in graph_inputs.items():
-            if name == ANSWER_NAME_FILE:
+        for nim, graph in graph_inputs.items():
+            if nim == ANSWER_NAME_FILE:
                 continue
             grade, _, _ = compare_graph(graph_answer, graph)
-            logging.info(f'{name} has a grade of {grade}')
+            results.append((exam_name, nim, grade))
+            logging.info(f'In {exam_name}, {nim} has a grade of {grade}')
+
+        with open(f'results.csv', 'w', encoding='UTF8', newline='') as exam_result:
+            writer = csv.writer(exam_result)
+            writer.writerow(HEADER)
+            for res in results:
+                writer.writerow(res)
 
 
 if __name__ == "__main__":
