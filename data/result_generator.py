733c02c978faee1bff8c990a4d3fa1730f01af87
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-05-20 20:05:57 +0700

    chore: change header to exam_name and number

--- data/result_generator.py
+++ data/result_generator.py
@@ -9,29 +9,30 @@ import logging
 
 ANSWER_NAME_FILE = 'answer'
 EXAM_DIRECTORIES = {
-    'Kuis 2 IA': 'Kuis_2_IF2210_2020\\IA',
-    'Kuis 2 IB': 'Kuis_2_IF2210_2020\\IB',
-    'KUIS 2 IC': 'Kuis_2_IF2210_2020\\IC',
-    'UTS IA': 'UTS_IF2210_2020\\IA',
-    'UTS IB': 'UTS_IF2210_2020\\IB'
+    ('Kuis 2', 'IA'): 'Kuis_2_IF2210_2020\\IA',
+    ('Kuis 2', 'IB'): 'Kuis_2_IF2210_2020\\IB',
+    ('KUIS 2', 'IC'): 'Kuis_2_IF2210_2020\\IC',
+    ('UTS', 'IA'): 'UTS_IF2210_2020\\IA',
+    ('UTS', 'IB'): 'UTS_IF2210_2020\\IB'
 }
-HEADER = ['exam_name', 'nim', 'grade']
+HEADER = ['exam_name', 'number', 'nim', 'grade']
 
 
 def main():
     logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
     parent_dir = os.path.dirname(os.path.abspath(__file__))
-    results: list[tuple[str, str, str]] = []
+    results: list[tuple[str, str, str, str]] = []
 
-    for exam_name, dir_exam in EXAM_DIRECTORIES.items():
-        logging.info(f'Comparing Answer for exam={exam_name}')
+    for exam, dir_exam in EXAM_DIRECTORIES.items():
+        exam_name, number = exam
+        logging.info(f'We got the {exam_name} and {number}')
         cur_dir = os.path.join(parent_dir, f'exams\\{dir_exam}')
         input_files = os.listdir(cur_dir)
         graph_inputs: dict[str, Graph] = {}
         for submission in input_files:
             nim = submission.split('.')[0]
             cur_path = os.path.join(cur_dir, submission)
-            logging.info(f'Generating CFG of {nim} submission in exam={exam_name}')
+            logging.info(f'Generating CFG of {nim} submission in exam={exam_name} - {number}')
             graph = Graph(get_cfg(cur_path))
             collapse(graph)
             graph_inputs[nim] = graph
@@ -44,8 +45,8 @@ def main():
             if nim == ANSWER_NAME_FILE:
                 continue
             grade, _, _ = compare_graph(graph_answer, graph)
-            results.append((exam_name, nim, grade))
-            logging.info(f'In {exam_name}, {nim} has a grade of {grade}')
+            results.append((exam_name, number, nim, grade))
+            logging.info(f'In {exam_name} - {number}, {nim} has a grade of {grade}')
 
         logging.info(f'Successfully generated {len(graph_inputs) - 1} submissions on exam={exam_name}')
 
