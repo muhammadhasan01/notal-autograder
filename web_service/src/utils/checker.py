6faf7cada4420492334902e35e333d60473262a5
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-06-01 20:16:27 +0700

    feat: add check request function

--- web_service/src/utils/checker.py
+++ web_service/src/utils/checker.py
@@ -1,3 +1,6 @@
+from http import HTTPStatus
+
+from flask import Request
 from werkzeug.datastructures import FileStorage
 
 from intermediate.src.classes.constants import Constants
@@ -14,3 +17,46 @@ def check_file(file: FileStorage):
     if file and not allowed_file(file.filename):
         return True, 'Extension file not supported'
     return False, ''
+
+
+def check_request(request: Request):
+    request_data = request.get_json()
+
+    # check request data is valid
+    mandatory_attributes = ['references', 'referencesFileNames', 'solution', 'solutionFileName', 'timeLimit']
+    if not request_data or not all(key in request_data for key in mandatory_attributes):
+        return (
+            True,
+            "invalid request body",
+            HTTPStatus.BAD_REQUEST
+        )
+    if not isinstance(request_data['references'], list):
+        return (
+            True,
+            "references must be a list of string",
+            HTTPStatus.BAD_REQUEST
+        )
+    if not isinstance(request_data['referencesFileNames'], list):
+        return (
+            True,
+            "referencesFileNames must be a list of string",
+            HTTPStatus.BAD_REQUEST
+        )
+    if not isinstance(request_data['solution'], str):
+        return (
+            True,
+            "solution must be a string",
+            HTTPStatus.BAD_REQUEST
+        )
+    if not isinstance(request_data['solutionFileName'], str):
+        return (
+            True,
+            "solutionFileName must be a string",
+            HTTPStatus.BAD_REQUEST
+        )
+    if not isinstance(request_data['timeLimit'], int):
+        return (
+            True,
+            "timeLimit must be an integer",
+            HTTPStatus.BAD_REQUEST
+        )
